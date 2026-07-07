a3bdf485784fea8680eb658770b14e93e2774d58
Shawn Chiu <142949485+Shawnhec@users.noreply.github.com>
2026-07-06 21:30:27 -0700

    Add: min-cost-to-connect-points - submission-6

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-6.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        Map<Integer, Set<int[]>> g = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            g.put(i, new HashSet<>());
+        }
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int d = distance(points[i], points[j]);
+                g.get(i).add(new int[]{j, d});
+                g.get(j).add(new int[]{i, d});
+            }
+        }
+
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        Set<Integer> visited = new HashSet<>();
+        int ans = 0; 
+        pq.offer(new int[]{0, 0});
+        
+        while(visited.size() < n) {
+            int[] leaving = pq.poll();
+            if(visited.contains(leaving[0])) continue;
+            visited.add(leaving[0]);
+            ans += leaving[1];
+            for(int[] neighbor : g.get(leaving[0])) {
+                pq.offer(neighbor);
+            }
+        }
+        return ans;
+    }
+
+    public int distance(int[] a, int[] b) {
+        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    }
+}
+
+
+
+/*
+[0,1,2,3]
+
+[0,1]
+[0,2]
+[0,3]
+
+[1,2]
+[1,3]
+
+[2,3]
+
+
+*/
