fb62afd1650d01a6119435931985fe4f19a51637
Shawn Chiu <142949485+Shawnhec@users.noreply.github.com>
2026-07-21 16:42:26 -0700

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.java
@@ -0,0 +1,51 @@
+/*
+T : O(n + klogn)
+S : O(n)
+*/
+// class Solution {
+//     public int[][] kClosest(int[][] points, int k) {
+//        Queue<double[]> que = new PriorityQueue<>((a, b) -> Double.compare(a[0], b[0]));
+//        int[][] ans = new int[k][2];
+//        for (int i = 0; i < points.length; i++) {                                    // T : O(n)
+//         que.add(new double[] {distance(points[i][0], points[i][1]), i});
+//        }
+
+
+//         for (int i = 0; i < k; i++) {                                               // T : O(klogn)
+//             ans[i] = points[(int) que.poll()[1]];
+//         }
+//         return ans;
+//     }
+
+
+
+//     public double distance(int x, int y) {
+//         return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+//     }
+// }
+
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+       Queue<double[]> que = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+       int[][] ans = new int[k][2];
+       for (int i = 0; i < points.length; i++) {                                    // T : O(n)
+        que.add(new double[] {distance(points[i][0], points[i][1]), i});
+        if(que.size() > k) {
+            que.poll();
+        }
+       }
+
+
+        for (int i = 0; i < k; i++) {                                               // T : O(klogn)
+            ans[i] = points[(int) que.poll()[1]];
+        }
+        return ans;
+    }
+
+
+
+    public double distance(int x, int y) {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+}
