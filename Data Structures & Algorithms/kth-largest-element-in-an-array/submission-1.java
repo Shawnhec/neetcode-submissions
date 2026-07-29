6d52d15733f462a0f67bd8f09b50650f552201fd
Shawn Chiu <142949485+Shawnhec@users.noreply.github.com>
2026-07-28 21:53:07 -0700

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        List<Integer> arr = new ArrayList<>();
+        for(int num : nums) arr.add(num);
+        return quickSelect(arr, k);
+    }
+
+    public int quickSelect(List<Integer> arr, int k) {
+        Random rand = new Random();
+        int pivot = arr.get(rand.nextInt(arr.size()));
+        
+        
+        List<Integer> less = new ArrayList<>();
+        List<Integer> greater = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        
+        for(int num : arr) {
+            if(num < pivot) less.add(num);
+            if(num == pivot) equal.add(num);
+            if(num > pivot) greater.add(num);
+        }
+
+        if(greater.size() >= k) {
+            return quickSelect(greater, k);
+        }
+
+        if(greater.size() + equal.size() < k) {
+            return quickSelect(less, k - greater.size() - equal.size());
+        }
+
+
+        return pivot; 
+    }
+}
+
+
+/*
+
+[2,3,1,5,4] k = 3
+
+pivot = 2
+
+[2, 3, 1] [4] [5]
+
+*/
