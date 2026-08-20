cede6a52b84a6c6a62a08baadfc1fddc5ba400cf
Shawn Chiu <142949485+Shawnhec@users.noreply.github.com>
2026-08-20 10:00:46 -0700

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int n = s.length();
+        Stack<Integer> left = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+        for(int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                left.push(i);
+            }
+            else if(c == '*') {
+                star.push(i);
+            }
+            else {
+                if(left.isEmpty() && star.isEmpty()) return false;
+                if(!left.isEmpty()) {
+                    left.pop();
+                } else {
+                    star.pop();
+                }
+            }
+        }
+
+
+        while(!left.isEmpty() && !star.isEmpty()) {
+            if(left.pop() > star.pop()) return false;
+        }
+
+        return left.isEmpty();
+    }
+}
+
+
+/*
+()*)
+
+leftMin = 1 
+leftMax = 2 
+
+(*))
+
+*/
